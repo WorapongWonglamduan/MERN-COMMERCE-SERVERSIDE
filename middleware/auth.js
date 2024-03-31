eeed4858933cde3a69a56743df232d20762fa5a0
WorapongWonglamduan <the_oo_ooo@hotmail.com>
2024-04-01 00:18:08 +0700

    token expire

--- middleware/auth.js
+++ middleware/auth.js
@@ -3,10 +3,13 @@ const jwt = require("jsonwebtoken");
 exports.auth = (req, res, next) => {
   try {
     const token = req.headers["authtoken"];
+
+    console.log("token =>", token);
     if (!token) {
       return res.status(401).send("No token, authorization denied");
     }
     const decode = jwt.verify(token, "jwtSecret");
+
     console.log("decode =>", decode);
 
     req.user = decode.user;
@@ -14,7 +17,11 @@ exports.auth = (req, res, next) => {
     // req.user = decode.user
     next();
   } catch (error) {
-    console.error(error);
+    console.error("error=>", error);
+
+    if (error instanceof jwt.TokenExpiredError) {
+      return res.status(401).send("Token has expired");
+    }
     return res.status(401).send("Token Invalid");
   }
 };
