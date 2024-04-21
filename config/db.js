d45f27aab2edec4c42f83d0f40fc8f5d4ff70846
WorapongWonglamduan <the_oo_ooo@hotmail.com>
2024-04-21 15:13:24 +0700

    up

--- config/db.js
+++ config/db.js
@@ -1,33 +1,34 @@
-const mongoose = require("mongoose");
-
-const connectDb = async () => {
-  try {
-    await mongoose.connect(process.env.DATABASE_DEPLOY).then((res) => {
-      console.log("connect DataBase success");
-    });
-  } catch (error) {
-    console.error(error);
-    process.exit(1);
-  }
-};
-
-module.exports = connectDb;
-//////////////////////////////////////////////////////
 // const mongoose = require("mongoose");
-// const mongoURI = process.env.DATABASE_DEPLOY; // Replace with your actual MongoDB URI
 
 // const connectDb = async () => {
 //   try {
-//     await mongoose.connect(mongoURI, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//       // bufferCommands: false,
+//     await mongoose.connect(process.env.DATABASE_DEPLOY).then((res) => {
+//       console.log("connect DataBase success");
 //     });
-//     console.log("Connected to MongoDB");
 //   } catch (error) {
-//     console.error("Error connecting to MongoDB:", error);
+//     console.error(error);
+//     process.exit(1);
 //   }
 // };
 
 // module.exports = connectDb;
+//////////////////////////////////////////////////////
+const mongoose = require("mongoose");
+const mongoURI = process.env.DATABASE_DEPLOY; // Replace with your actual MongoDB URI
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      // bufferCommands: false,
+    });
+    console.log("Connected to MongoDB");
+  } catch (error) {
+    process.exit(1);
+    console.error("Error connecting to MongoDB:", error);
+  }
+};
+
+module.exports = connectDb;
 ///////////////////////////////////
